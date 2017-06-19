3129b89a3c2647f8f5677f9df7b4c6ccfaad8f97
ingostramm@gmail.com <0gn11976>
2017-06-19 16:52:46 -0300

    Removendo Sujeira
    Removendo códigos não usados

--- laf/functions/init.php
+++ laf/functions/init.php
@@ -4,4 +4,3 @@ require_once 'general.php';
 require_once 'comments.php';
 require_once 'woocommerce.php';
 require_once 'protect_pages.php';
-// require_once 'update.php';
--- laf/functions/theme-updates-checker.php
+++
@@ -1,5 +0,0 @@
-<?php
-$update_checker = new ThemeUpdateChecker(
-	'flatsome-child',									// Theme folder name, AKA "slug". 
-	'http://updates.convertefacil.com.br/info.json'		// URL of the metadata file.
-);
