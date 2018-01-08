d7d5f11f48b2d34d3788e1071f857fd77374bdda
Ingo Stramm <ingostramm@gmail.com>
2018-01-08 10:52:35 -0200

    Versão Legado
    Incluindo os arquivos do Grunt por segurança. Os arquivos style.css e script.js antigos receberam o sufixo "-legacy" para não serem sobescritos.

--- laf/functions/general.php
+++ laf/functions/general.php
@@ -4,8 +4,10 @@
 add_action( 'wp_enqueue_scripts', 'cf_enqueue_styles' );
 
 function cf_enqueue_styles() {
-	wp_enqueue_style( 'cf-style', get_stylesheet_directory_uri() . '/assets/css/style.css', array(), '0.0.1' );
-	wp_enqueue_script( 'cf-script', get_stylesheet_directory_uri() . '/assets/js/main.min.js', array( 'jquery', 'jquery-mask' ), '0.0.1', true );
+	wp_enqueue_style( 'cf-style-legacy', get_stylesheet_directory_uri() . '/assets/css/style-legacy.css', array(), '1.0.0' );
+	wp_enqueue_script( 'cf-script-legacy', get_stylesheet_directory_uri() . '/assets/js/main-legacy.min.js', array( 'jquery', 'jquery-mask' ), '1.0.0', true );
+	wp_enqueue_style( 'cf-style', get_stylesheet_directory_uri() . '/assets/css/style.css', array( 'cf-style-legacy' ), '0.0.1' );
+	wp_enqueue_script( 'cf-script', get_stylesheet_directory_uri() . '/assets/js/main.min.js', array( 'jquery', 'jquery-mask', 'cf-script-legacy' ), '0.0.1', true );
 }
 
 // Altera a versão do jQuery usada pelo Wordpress
